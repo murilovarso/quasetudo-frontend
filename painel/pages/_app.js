10fc661c3269f3028e36272c13ab441522119dc4
murilovarso <murilovarso@gmail.com>
2022-06-29 00:58:19 -0300

    initial versio

---
+++ painel/pages/_app.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../css/style.css'
+
+const App = ({ Component, pageProps }) => {
+  return (
+    <div>
+      <Component {...pageProps} />
+    </div>
+  )
+}
+export default App
